d807d6a76f10311d119ed55f04f4c11aed9c15b7
Ze Mendes <zepedrosmendes@gmail.com>
2023-02-14 17:07:11 +0000

    added the validation methods for the file

--- lab01/VerificationInterface.java
+++ lab01/VerificationInterface.java
@@ -1,6 +1,6 @@
 import java.io.File;
 
 public interface VerificationInterface {
-    public void validateWordSearch(File f);
-    public void validateWords(File f);
+    public boolean validateWordSearch(File f);
+    public boolean validateWords(File f);
 }
--- lab01/WSSolver.java
+++ lab01/WSSolver.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 import java.io.File;
+import java.io.FileNotFoundException;
 
-public class WSSolver {
+public class WSSolver implements VerificationInterface, InfoInterface {
     public static void main(String[] args) throws Exception {
         String[] words =  {"Up", "Down", "Left", "Right", "UpLeft", "UpRight", "DownLeft", "DownRight"};
         char[][] puzzle = new char[40][40];
@@ -30,6 +31,93 @@ public class WSSolver {
             System.out.print("\n");
         }
 
+    }
+
+    @Override
+    public boolean validateWordSearch(File f) {
+
+        try {
+            Scanner sc = new Scanner(f);
+            String tempLine = sc.nextLine();
+            if(tempLine == null || !isStringUpperCase(tempLine)){
+                sc.close();
+                return false;
+            }
+
+            int ws_lenght = tempLine.length();
+            for(int i = 1; i < ws_lenght; i++){
+                tempLine = sc.nextLine();
+                if(tempLine.length() != ws_lenght || !isStringUpperCase(tempLine)){
+                    sc.close();
+                    return false;
+                }
+            }
+            sc.close();
+            return true;
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            return false;
+        }
+
+        
+    }
+
+    @Override
+    public boolean validateWords(File f) {
+        try {
+            Scanner sc = new Scanner(f);
+            String tempLine = sc.nextLine();
+            int ws_lenght = tempLine.length();
+            for(int i = 1; i < ws_lenght; i++)
+                tempLine = sc.nextLine();
+            
+            StringBuilder str = new StringBuilder();
+            while(sc.hasNextLine())
+                str.append(sc.nextLine());
+            sc.close();
+
+            String s = str.toString();
+
+            s = s.replace(" ", "");
+            s = s.replace(";", "");
+            
+            if(!s.matches("[a-zA-Z]+"))
+                return false;
+            return true;
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            return false;
+        }
+        
+    }
+
+    private static boolean isStringUpperCase(String s){
+        
+        //convert String to char array
+        char[] charArray = s.toCharArray();
+        
+        for(char c : charArray){
+            
+            //if any character is not in upper case, return false
+            if(!Character.isUpperCase(c))
+                return false;
+        }
+        
+        return true;
+    }
+
+    @Override
+    public void getWordSearch(File f) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void getWords(File f) {
+        // TODO Auto-generated method stub
+        
     }
 
             
